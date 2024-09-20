c14fd89a9fd3c9d7f01c81f393bfb2eb3cb0d3c3
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2024-09-20 10:33:45 -0400

    Update concat_mc.py

--- concat_mc.py
+++ concat_mc.py
@@ -1,1 +1,27 @@
+#Shay VanLandschoot
+#9/20/24
+#Concatenation Check
 
+
+#task 1
+
+name = 'Shay '
+fav_movie = 'guardians of the galaxy '
+
+print('My name is ' + name + 'And my favorit movie is ' + fav_movie + '.' )
+
+
+#task 2
+
+one = 'you use the cancatenation oporator to add thing together.'
+two = 'its harder then f stirings '
+three = 'you dont add anything to your variable when you add it to the concatenation '
+
+
+print("what i have learn about concatrnation is that " + one + two + 'and ' + three)
+ 
+
+ #task 3
+whopper = float(15.49)
+
+print('at burger king the cost of a whopper is ' + '$' + whopper + '.')
